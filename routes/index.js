e4fa8c3ca84b744d2fa80c5e7a25a5da13becf07
shraddharocks6 <shraddha6pandey@gmail.com>
2021-04-10 13:22:26 +0530

    three setup.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/three', function(req, res, next) {
+  res.render("d3.ejs");
+});
+
 module.exports = router;
